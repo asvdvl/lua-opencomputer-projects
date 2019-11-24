eb35706a51b850ca47125ef0c4cf30b5fe98887d
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2019-11-24 20:15:20 +0300

    set weather on clean and day
    set weather on clean and day. require debug card.

---
+++ weather.lua
@@ -0,0 +1,49 @@
+local cmp = require("component")
+local term = require("term")
+local debug = cmp.debug
+local world = debug.getWorld()
+local killedRain = 0
+local killedNight = 0
+local skipedTime = 0
+
+function killRain()
+  if (world.isRaining(true)) then
+    world.setRaining(false)
+    killedRain = killedRain + 1
+  end
+end
+
+function killNight()
+  local time = world.getTime()
+  if(time%24000 > 12000 and time%24000 < 24000) then
+    killedNight = killedNight + 1
+    world.setTime(time+(25000-time))  
+    skipedTime = skipedTime + (time+(2500-time))  
+  end
+end
+
+function printInfo()
+  term.clear()
+  local time = world.getTime()
+  print("kill rain: "..killedRain)
+  print("kill night: "..killedNight)
+  print("Skip time: "..skipedTime)
+  print("Full time: "..time)
+  print("Day time: "..time%24000)
+  local secInGame = time/20
+  
+  print(
+        "In game: "
+        ..math.floor(secInGame/3600)
+        ..":"..math.floor(secInGame/60%60)
+        ..":"..math.floor(secInGame%60)
+       )
+
+end
+
+while(true) do
+  killRain()
+  killNight()
+  printInfo()  
+  os.sleep(1)
+end
