b0f304318ab56e684750d979a0030c2332bda157
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2019-11-24 20:01:28 +0300

    reactor control
    reactor control, controlling reactor of BigReactors mod. connecting to computer port on reactor and need connect notify interface from random things(you can cut some code to fix it)

---
+++ reactor_control.lua
@@ -0,0 +1,50 @@
+local cmp = require("component")
+local reactor = cmp.br_reactor
+local RS = cmp.redstone
+local itemOnTurnOn = "minecraft:redstone_torch"
+local itemOnTurnOff = "minecraft:lever"
+local messageOnTurnOn = "реактор включен"
+local messageOnTurnOff = "реактор выключен"
+local notifI = cmp.notification_interface
+local percentCharge
+local rodLevel
+local valueMinimum = 20
+local valueMaximum = 80
+local retentionMode = false
+
+function turnOn()
+  reactor.setActive(true)
+  reactor.setAllControlRodLevels(0)
+  notifI.notify(messageOnTurnOn, "", itemOnTurnOn, 0)
+  retentionMode = false
+end
+
+function turnOff()
+  reactor.setActive(false)
+  reactor.setAllControlRodLevels(0)
+  notifI.notify(messageOnTurnOff, "", itemOnTurnOff, 0)
+  retentionMode = false
+end
+
+
+while true do
+  percentCharge = ((reactor.getEnergyStored()/reactor.getEnergyCapacity())*100)
+  if percentCharge > valueMaximum and reactor.getActive() then
+    turnOff()
+  elseif percentCharge < valueMinimum and reactor.getActive() ~= true then
+    turnOn()
+  elseif percentCharge < (valueMaximum - 5) and percentCharge > (valueMinimum + 5) then
+  --powen regulation
+    if reactor.getActive() ~= true then
+      turnOn()
+    end
+    rodLevel = (((percentCharge - (valueMinimum+5))/((valueMaximum-5) - (valueMinimum+5)))*100)
+    RS.setOutput(3, 150/rodLevel)
+    reactor.setAllControlRodLevels(rodLevel)
+    if retentionMode ~= true then
+      retentionMode = true
+      notifI.notify("режим стабилизации", "", "minecraft:obsidian", 0)
+    end
+  end
+  os.sleep(0.1)
+end
