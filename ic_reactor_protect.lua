c451f7475d7095da875c08a9493d6c55dca027dc
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-06-29 23:05:42 +0300

    Update ic_reactor_protect.lua
    add more variables for connect to reactor
    add catch error on read data

--- ic_reactor_protect.lua
+++ ic_reactor_protect.lua
@@ -1,24 +1,67 @@
 --user settings
 local sideOut = 0
-local sideIn = 0
-local alarm = true
-local sideAlarm = 0
+local sideIn = 1
+local alarm = false
+local sideAlarm = 4
+local connectType = 1
+--0 - connect to reactor chamber
+--1 - connect to core
+--2 - connect by id
+local reactor_id = "" --fill this if connectType = 2 (you can fill unique start of address e.g. "6afd")
 --end user settings
 
 local cmp = require("component")
-local reactor = cmp.reactor_chamber
-local rs = cmp.proxy(cmp.get("reactor"))
-local rs1 = cmp.proxy(cmp.get("leverandalarm"))
+local rs = cmp.proxy(cmp.get("6a9"))
+local rs1 = cmp.proxy(cmp.get("30a"))
 local counter = 0
+local objects = {}
 
-function printInfo()
+function printInfo(data)
 	require("term").clear()
-	print("Reactor heat: "..tostring(reactor.getHeat()/reactor.getMaxHeat()*100).."%")
-	print("Reactor output: "..reactor.getReactorEUOutput().." EU")
+	print("Reactor heat: "..tostring(data.heat/data.maxHeat*100).."%")
+	print("Reactor output: "..data.reactorEUOutput.." EU")
+end
+
+function getInfo()
+	local data = {
+	heat = 0,
+	maxHeat = 0,
+	reactorEUOutput = 0
+	}	
+	
+	if objects.reactor == nil then
+		if connectType == 0 then
+			objects.reactor = cmp.reactor_chamber
+		elseif connectType == 1 then
+			objects.reactor = cmp.reactor
+		elseif connectType == 2 then
+			objects.reactor = cmp.proxy(cmp.get(reactor_id))
+		else
+			print("error connectType")
+			os.sleep(5);
+			os.exit();
+		end
+	end
+	
+	data.heat = objects.reactor.getHeat()
+	data.maxHeat = objects.reactor.getMaxHeat()
+	data.reactorEUOutput = objects.reactor.getReactorEUOutput()
+	
+	return data
 end
 
 while true do
-	if (reactor.getHeat()/reactor.getMaxHeat()*100 >= 80.0) then
+	local succes, data = pcall(getInfo)
+	if not succes then
+		print("Error", data)
+		rs.setOutput(sideOut, 0)
+		if alarm then
+			rs1.setOutput(sideAlarm, 15)
+		end
+		os.exit();
+	end
+
+	if (data.heat/data.maxHeat*100 >= 80.0) then
 		rs.setOutput(sideOut, 0)
 		if alarm then
 			rs1.setOutput(sideAlarm, 15)
@@ -34,7 +77,7 @@ while true do
 		end
 	end
 	if counter == 3 then
-		printInfo()
+		printInfo(data)
 		counter = 0
 	end
 	counter = counter + 1
