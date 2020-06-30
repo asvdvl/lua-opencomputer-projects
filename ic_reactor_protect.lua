6309b7bd9eea8a66ef1a09dbc8b61865f1947710
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-06-30 23:32:59 +0300

    Update ic_reactor_protect.lua
    add reactor state
    set resolution with start program

--- ic_reactor_protect.lua
+++ ic_reactor_protect.lua
@@ -2,31 +2,39 @@
 local sideOut = 0
 local sideIn = 1
 local alarm = false
-local sideAlarm = 4
-local connectType = 1
+local sideAlarm = 2
+local connectType = 0
 --0 - connect to reactor chamber
 --1 - connect to core
 --2 - connect by id
-local reactor_id = "" --fill this if connectType = 2 (you can fill unique start of address e.g. "6afd")
+local reactor_id = "0" --fill this if connectType = 2 (you can fill unique start of address e.g. "6afd")
 --end user settings
 
 local cmp = require("component")
-local rs = cmp.proxy(cmp.get("6a9"))
-local rs1 = cmp.proxy(cmp.get("30a"))
+local rs = cmp.proxy(cmp.get("000"))
+local rs1 = cmp.proxy(cmp.get("111"))
 local counter = 0
 local objects = {}
 
 function printInfo(data)
 	require("term").clear()
 	print("Reactor heat: "..tostring(data.heat/data.maxHeat*100).."%")
-	print("Reactor output: "..data.reactorEUOutput.." EU")
+	print("Reactor output: "..math.floor(data.reactorEUOutput).." EU")
+	local reactorStateString = "";
+	if data.reactorState then
+		reactorStateString = "Enable"
+	else
+		reactorStateString = "Disable"
+	end
+	print("Reactor state: "..reactorStateString)
 end
 
 function getInfo()
 	local data = {
 	heat = 0,
 	maxHeat = 0,
-	reactorEUOutput = 0
+	reactorEUOutput = 0,
+	reactorState = false
 	}	
 	
 	if objects.reactor == nil then
@@ -46,10 +54,12 @@ function getInfo()
 	data.heat = objects.reactor.getHeat()
 	data.maxHeat = objects.reactor.getMaxHeat()
 	data.reactorEUOutput = objects.reactor.getReactorEUOutput()
+	data.reactorState = objects.reactor.producesEnergy()
 	
 	return data
 end
 
+cmp.gpu.setResolution(25, 5)
 while true do
 	local succes, data = pcall(getInfo)
 	if not succes then
