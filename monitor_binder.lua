1f5e08d060eadaa869e0eb146451bf683c6abd13
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-07-09 16:42:49 +0300

    Add files via upload

---
+++ monitor_binder.lua
@@ -0,0 +1,17 @@
+local cmp = require("component") 
+--gpu, screen
+local binds = {
+{"b078", "6d14"},
+{"f93", "5c6"},
+}
+
+for id, _ in cmp.list("gpu") do 
+	for _, addr in pairs(binds) do 
+		if not cmp.get(addr[1]) and not cmp.get(addr[2]) then
+			local gpu = cmp.proxy(cmp.get(addr[1]))
+			gpu.bind(cmp.get(addr[2]))
+			gpu.setResolution(gpu.maxResolution())
+		end
+	end
+end
+require("term").clear()
