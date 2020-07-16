cf1b38ce081ff6b49bbd4ff2d2d88c24973d2088
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-07-16 13:31:48 +0300

    Create settings.lua
    init

---
+++ libs/settings.lua
@@ -0,0 +1,15 @@
+local srl = require("serialization")
+local settings = {}
+
+
+function settings.getSettings(pathToFile, defaultSettings)
+	
+	return deSerSettings
+end
+
+function settings.setSettings(pathToFile, newSettings)
+
+	return success
+end
+
+return settings
