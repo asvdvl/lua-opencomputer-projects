6b98191af58a8b806cf834571432d8b66ddd51ed
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-07-29 14:18:06 +0300

    init commit

---
+++ libs/time.lua
@@ -0,0 +1,24 @@
+local time = {}
+local filePath = "/tmp/.time"
+
+function getCurrent()
+
+end
+
+function time.getUNIX()
+
+end
+
+function time.getBySpecificFormat(format)
+
+end
+
+function time.getTime()
+
+end
+
+function time.getDate()
+
+end
+
+return time
