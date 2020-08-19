0bb5970090eac2c7c59c2cc41b30fa3a0bbaeb97
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-08-20 00:49:10 +0300

    Update settings.lua
    add file extension
    add comments
    rename pathToFile to settingsFileName
    add verify structure by default
    replace stupid var == nil/var ~=nil on not var/var

--- libs/settings.lua
+++ libs/settings.lua
@@ -2,64 +2,105 @@ local settings = {}
 local srl = require("serialization")
 local fs = require("filesystem")
 local defaultFileLocation = "/etc/settings/"
+local defaultFileExtension = ".set"
 
-function checkPath(pathToFile)
-	checkArg(1, pathToFile, "string")
-	
-	if string.sub(pathToFile, 1, 1) ~= "/" then
-		pathToFile = fs.concat(defaultFileLocation, pathToFile)
+function checkPath(settingsFileName)
+	--check on absolute path
+	if string.sub(settingsFileName, 1, 1) ~= "/" then
+		settingsFileName = fs.concat(defaultFileLocation, settingsFileName)
 	end
 	
-	if not fs.exists(fs.path(pathToFile)) then
-		local status, reason = fs.makeDirectory(fs.path(pathToFile))
+	--check file extention
+	if string.sub(settingsFileName, -#defaultFileExtension) ~= defaultFileExtension then
+		settingsFileName = settingsFileName..defaultFileExtension
+	end
+	
+	--check and create path to file (no create file)
+	if not fs.exists(fs.path(settingsFileName)) then
+		local status, reason = fs.makeDirectory(fs.path(settingsFileName))
 		if not status then
 			return status, reason
 		end
 	end
 	
-	return pathToFile
+	return settingsFileName
 end
 
-function settings.getSettings(pathToFile, defaultSettings)
-	pathToFile, reason = checkPath(pathToFile)
+function verifyAndCorrectStructure(fileTab, defTab)
+	fileTab = setmetatable(fileTab, {__index = defTab})
+	fileTabNew = {}
+	for key, val in pairs(defTab) do 
+		if not rawget(fileTab, key) then
+			needRewrite = true
+		end
+		fileTabNew[key] = fileTab[key] 
+	end
+	return fileTabNew, needRewrite
+end
 
-	if reason ~=nil then
+function settings.getSettings(settingsFileName, defaultSettings, dontCorrectStructure)
+	checkArg(1, settingsFileName, "string")
+	settingsFileName, reason = checkPath(settingsFileName)
+	
+	--check if checkPath return error
+	if reason then
 		return false, reason
 	end
 	
-	if not fs.exists(pathToFile) then
-		local status, reason = settings.setSettings(pathToFile, defaultSettings)
+	--creck file and create if exist defaultSettings
+	if not fs.exists(settingsFileName) and defaultSettings then
+		local status, reason = settings.setSettings(settingsFileName, defaultSettings)
 		if not status then
 			return false, reason
 		end
 	end
 	
-	local file, reason = io.open(pathToFile)
+	--open file
+	local file, reason = io.open(settingsFileName)
 	if type(file) ~= "table" then
 		return false, reason
 	end
 	
+	--read and unserialize file
 	local serialized = file:read("*a")
 	file:close()
 	local var, reason = srl.unserialize(serialized)
 	
-	if reason ~=nil then
+	--correct structure with defaultSettings
+	if not reason and type(var) == "table" and not dontCorrectStructure then
+		local needRewrite = false
+		var, needRewrite = verifyAndCorrectStructure(var, defaultSettings)
+		if needRewrite then
+			local status, reason = settings.setSettings(settingsFileName, var)
+			if not status then
+				return false, reason
+			end
+		end
+	end
+
+	if reason then
 		return false, reason
 	else
 		return true, var
 	end
 end
 
-function settings.setSettings(pathToFile, newSettings)
+function settings.setSettings(settingsFileName, newSettings)
 	checkArg(2, newSettings, "nil", "boolean", "number", "string", "table")
-	pathToFile, reason = checkPath(pathToFile)
-	if reason ~=nil then
+	
+	--check if checkPath return error
+	settingsFileName, reason = checkPath(settingsFileName)
+	if reason then
 		return false, reason
 	end
-	local file, reason = io.open(pathToFile, "w")
+	
+	--open file
+	local file, reason = io.open(settingsFileName, "w")
 	if type(file) ~= "table" then
 		return false, reason
 	end
+	
+	--write to file
 	file:write(srl.serialize(newSettings))  --DATA
 	file:close()
 	
