c17b6188a41c475b5345f608900e6a6a3d091bcd
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-08-26 16:06:54 +0300

    Update simply_background_receiver.lua
    add support computronics chat upgrade(or chat box)

--- soft/simply_background_receiver.lua
+++ soft/simply_background_receiver.lua
@@ -3,10 +3,19 @@ local event = require("event")
 
 cmp.modem.open(20)
 
+local chatAvailable = cmp.isAvailable("chat")
+
 function receive(...)
 	local param = {...}
 	print(os.date()..tostring(param[6]))
+	if chatAvailable then
+		cmp.chat.say(os.date().." "..tostring(param[6]))
+	end
 	require("computer").beep(1400, 0.2)
 end
 
 event.listen("modem_message", receive)
+if chatAvailable then
+	cmp.chat.setName("system alert")
+	cmp.chat.say("Init complete")
+end
