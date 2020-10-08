e348b1f85a96f933f60675448c38308bd5c2d0b7
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-10-09 00:20:20 +0300

    Added saving settings to file.

--- soft/ic_reactor_protect.lua
+++ soft/ic_reactor_protect.lua
@@ -1,31 +1,48 @@
---user settings
-local sideOut = 0
-local sideIn = 1
-local alarm = false
-local sideAlarm = 2
-local connectType = 0
---0 - connect to reactor chamber
---1 - connect to core
---2 - connect by id
-local reactorId = "0" --fill this if connectType = 2 (you can fill unique start of address e.g. "6afd")
-local reactorRedstone = "000" --you can fill unique start of address e.g. "6afd"
-local inputRedstone = "111" --you can fill unique start of address e.g. "6afd"
---energy stogares
-local useEnergyStorage = true
-local energyStorageType = 0
---0 - use gregtech energy buffer
---1 - use ic2 energy storage
---2 - use gregtech energy buffer with asielib(nuclear output)
-local connectToEnergyStorageType = 0 
---0 - connect by component name
---1 - connect by address
-local energyStorageAddress = "0" --fill this if connectToEnergyStorageType = 1 (you can fill unique start of address e.g. "6afd")
-local updatePer = 3 -- 0 = never
---end user settings
+local defaultSettings = {
+	sideOut = 0,
+	sideIn = 1,
+	alarm = false,
+	sideAlarm = 2,
+	connectToReactorType = 0,
+	--0 - connect to reactor chamber
+	--1 - connect to core
+	--2 - connect by id
+	reactorId = "0", --fill this if connectToReactorType = 2 (you can fill unique start of address e.g. "6afd")
+	reactorRedstone = "111", --you can fill unique start of address e.g. "6afd"
+	inputRedstone = "222", --you can fill unique start of address e.g. "6afd"
+	--energy stogares
+	useEnergyStorage = false,
+	energyStorageType = 0,
+	--0 - use gregtech energy buffer
+	--1 - use ic2 energy storage
+	--2 - use gregtech energy buffer with asielib(nuclear output)
+	connectToEnergyStorageType = 0, 
+	--0 - connect by component name
+	--1 - connect by address
+	energyStorageAddress = "0", --fill this if connectToEnergyStorageType = 1 (you can fill unique start of address e.g. "6afd")
+	updatePer = 3, -- 0 = never
+}
+
+local settLib = require("settings")
+local settings = {}
+
+--load settings
+require("term").clear()
+io.stdout:write("loading settings\n")
+local status
+status, settings = settLib.getSettings("reactor", defaultSettings)
+if status then
+	io.stdout:write("loading complete\n")
+else
+	io.stderr:write("error loading settings: "..settings.."\n")
+	settings = defaultSettings
+	os.sleep(3)
+end
 
 local cmp = require("component")
-local rs = cmp.proxy(cmp.get(reactorRedstone))
-local rs1 = cmp.proxy(cmp.get(inputRedstone))
+local term = require("term")
+local rs = cmp.proxy(cmp.get(settings.reactorRedstone))
+local rs1 = cmp.proxy(cmp.get(settings.inputRedstone))
 local counter = 0
 local objects = {}
 local delay = 0.1
@@ -43,8 +60,8 @@ local programStateString = {
 --3 - overheat reactor
 --4 - no redstone input
 
-function printInfo(data)
-	require("term").clear()
+local function printInfo(data)
+	term.clear()
 	print("Reactor heat: "..tostring(data.heat/data.maxHeat*100).."%")
 	print("Reactor output: "..math.floor(data.reactorEUOutput).." EU")
 	local reactorStateString = "";
@@ -55,12 +72,12 @@ function printInfo(data)
 	end
 	print("Reactor state: "..reactorStateString)
 	print("Program state: "..tostring(programState))
-	if useEnergyStorage then
-		print("Energy storage: "..tostring(math.floor(data.batBuf/data.batBufMax*100)))
+	if settings.useEnergyStorage then
+		print("Energy storage: "..tostring(math.floor(data.batBuf/data.batBufMax*100)).."%")
 	end
 end
 
-function getInfo()
+local function getInfo()
 	local data = {
 	heat = 0,
 	maxHeat = 0,
@@ -71,15 +88,15 @@ function getInfo()
 	}	
 
 	if objects.reactor == nil then
-		if connectToReactorType == 0 then
+		if settings.connectToReactorType == 0 then
 			--0 - connect to reactor chamber
 			objects.reactor = cmp.reactor_chamber
-		elseif connectToReactorType == 1 then
+		elseif settings.connectToReactorType == 1 then
 			--1 - connect to core
 			objects.reactor = cmp.reactor
-		elseif connectToReactorType == 2 then
+		elseif settings.connectToReactorType == 2 then
 			--2 - connect by id
-			objects.reactor = cmp.proxy(cmp.get(reactorId))
+			objects.reactor = cmp.proxy(cmp.get(settings.reactorId))
 		else
 			io.stderr:write("error connectToReactorType")
 			os.sleep(5);
@@ -87,36 +104,36 @@ function getInfo()
 		end
 	end
 
-	if useEnergyStorage == true and objects.batterybuffer == nil then
-		function getStorage(byAddress, addressOrName)
+	if settings.useEnergyStorage and not objects.batterybuffer then
+		local function getStorage(byAddress, name, address)
 			if byAddress == 1 then
-				return cmp.proxy(cmp.get(addressOrName))
+				return cmp.proxy(cmp.get(address))
 			else
-				return cmp.proxy(cmp.list(addressOrName)())
+				return cmp.proxy(cmp.list(name)())
 			end
 		end
 		
-		if energyStorageType == 0 then
+		if settings.energyStorageType == 0 then
 			--0 - use gregtech energy buffer
-			objects.batterybuffer = getStorage(connectToEnergyStorageType, "batterybuffer")
+			objects.batterybuffer = getStorage(settings.connectToEnergyStorageType, "batterybuffer", settings.energyStorageAddress)
 			objects.getBatBuf = objects.batterybuffer.getStoredEU
 			objects.getBatBufMax = objects.batterybuffer.getEUCapacity
 			
-		elseif energyStorageType == 1 then
+		elseif settings.energyStorageType == 1 then
 			--1 - use ic2 energy storage
-			function zeroReturnFunction()
+			local function zeroReturnFunction()
 				return 0
 			end
 			objects.getBatBuf = zeroReturnFunction
 			objects.getBatBufMax = zeroReturnFunction
 			
-		elseif energyStorageType == 2 then
+		elseif settings.energyStorageType == 2 then
 			--2 - use gregtech energy buffer with asielib(nuclear output)
-			objects.batterybuffer = getStorage(connectToEnergyStorageType, "batterybuffer")
-			function getStoredEU()
+			objects.batterybuffer = getStorage(settings.connectToEnergyStorageType, "batterybuffer", settings.energyStorageAddress)
+			local function getStoredEU()
 				return string.gsub(objects.batterybuffer.getSensorInformation()[3], "([^0-9]+)", "")
 			end
-			function getEUCapacity()
+			local function getEUCapacity()
 				return string.gsub(objects.batterybuffer.getSensorInformation()[4], "([^0-9]+)", "")
 			end
 			objects.getBatBuf = getStoredEU
@@ -138,29 +155,42 @@ function getInfo()
 	return data
 end
 
+term.clear()
+--load settings
+print("loading settings")
+local status
+status, settings = settLib.getSettings("reactor", defaultSettings)
+if status then
+	io.stdout:write("loading complete\n")
+else
+	io.stderr:write("error loading settings: "..settings.."\n")
+	settings = defaultSettings
+	os.sleep(3)
+end
+
 cmp.gpu.setResolution(24, 6)
 while true do
 	local succes, data = pcall(getInfo)
 	if not succes then
 		programState = 2
 		io.stderr:write("Error", data)
-		rs.setOutput(sideOut, 0)
-		if alarm then
-			rs1.setOutput(sideAlarm, 15)
+		rs.setOutput(settings.sideOut, 0)
+		if settings.alarm then
+			rs1.setOutput(settings.sideAlarm, 15)
 		end
 		os.exit();
 	end
 
-	if rs1.getInput(sideIn) == 0 then
+	if rs1.getInput(settings.sideIn) == 0 then
 		--no redstone
 		programState = 4
 	elseif (data.heat/data.maxHeat*100 >= 80.0) then
 		--overheat
 		programState = 3
-		if alarm then
-			rs1.setOutput(sideAlarm, 15)
+		if settings.alarm then
+			rs1.setOutput(settings.sideAlarm, 15)
 		end
-	elseif useEnergyStorage == true and data.batBuf/data.batBufMax*100 >=90 then
+	elseif settings.useEnergyStorage == true and data.batBuf/data.batBufMax*100 >=90 then
 		--full batbuffer
 		programState = 1
 	else
@@ -168,12 +198,12 @@ while true do
 	end
 	
 	if programState == 0 then
-		rs.setOutput(sideOut, rs1.getInput(sideIn))
+		rs.setOutput(settings.sideOut, rs1.getInput(settings.sideIn))
 	else
-		rs.setOutput(sideOut, 0)
+		rs.setOutput(settings.sideOut, 0)
 	end
 	
-	if counter == updatePer then
+	if counter == settings.updatePer then
 		printInfo(data)
 		counter = 0
 	end
