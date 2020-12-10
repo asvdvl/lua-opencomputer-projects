db438bfe5be3539c403f64e8c49bca42768d7029
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-12-10 21:48:11 +0300

    Added fullscreen mode Added colour output

--- soft/TPSCalculator.lua
+++ soft/TPSCalculator.lua
@@ -1,7 +1,20 @@
 local time = require("time")
+local cmp = require("component")
+local gpu = cmp.gpu
+
+local screenW = 9
+local screenH = 3
+local calculateTicks = 20    --5 ticks - closest to real value and low latency. 20 ticks - 1 second
+local colorOutput = true
+local fullsreen = true
 
 local function getTicks()
     return os.time()/3.6
+end
+
+if fullsreen then
+    require("term").clear()
+    gpu.setResolution(screenW, screenH)
 end
 
 while true do
@@ -9,10 +22,22 @@ while true do
     local gameTime = getTicks()
 
     --sleep on in-game ticks
-    --5 ticks - closest to real value and low latency
-    while (getTicks() - gameTime) < 5 do
+    while (getTicks() - gameTime) < calculateTicks do
         os.sleep(0)
     end
+    local tps = math.ceil(10000/((time.getRaw() - realTime)/(getTicks() - gameTime)))/10
 
-    print("tps", 1000/((time.getRaw() - realTime)/(getTicks() - gameTime)))
+    --set collor
+    if colorOutput then
+        if tps <= 12 then
+            gpu.setForeground(0xFF0000)
+        elseif tps <= 15 then
+            gpu.setForeground(0xFFFF55)
+        else
+            gpu.setForeground(0x55FF55)
+        end
+    end
+
+    print("")
+    print("tps: "..tps)
 end
