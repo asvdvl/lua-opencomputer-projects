16cba0b76c0d2582d6f11730e332a6422186c5db
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2021-02-01 23:38:53 +0300

    added light control added keyboard keycodes rewrite object table added print menu added key event handler

--- soft/spawner_control.lua
+++ soft/spawner_control.lua
@@ -1,61 +1,109 @@
 local cmp = require("component")
-local AirRS = cmp.proxy(cmp.get("3ff"))
-local SpawnerRS = cmp.proxy(cmp.get("4f2"))
 local term = require("term")
+local exitF
+local eventNumb = 0
+local light = {state = false}
+local keycodes = { --4 parameter of key_down and key_up events
+	{2, "1"}, 	--{keycode, "real key"}
+	{3, "2"},
+	{4, "3"},
+	{5, "4"},
+	{6, "5"},
+	{7, "6"},
+	{8, "7"},
+	{9, "8"},
+	{10, "9"},
+	{11, "0"},
+	{38, "l"},
+	{16, "q"}
+}
 
-local object = {
-    ifrits  	= {side = 5, state = 0},
-    enderman 	= {side = 4, state = 0},
-	witherSkel 	= {side = 3, state = 0},
-	light    	= {state = true},
-	fun			= {side = 0, state = 0}
+--user settings
+local objects = {
+	--name - visible name
+	--redstone - address of redstone interface
+	--key - real keyboard key(see keycodes table)
+	--side - side of redstone interface
+	--state - start redstone state
+    {name = "ifrits", redstone = "d31", key = "1", side = 5, state = false},
+    {name = "endermans", redstone = "d31", key = "2", side = 4, state = false},
+	{name = "wither skelets", redstone = "d31", key = "3", side = 3, state = false},
+	{name = "funs", redstone = "d31", key = "4", side = 0, state = true}
 
-    }
+}
+--end user settings
 
-function RScontrolSet(obj) 
-	
-	if obj.state then 
-		obj.state = 0 
-	else 
-		obj.state = 1 
-	end 
-	
-	SpawnerRS.setOutput(obj.side, obj.state)
+local function turnLight()
+	local brightness = 0
+	if light.state then
+		brightness = 0
+	else
+		brightness = 15
+	end
+	light.state = not light.state
+	for addr in pairs(cmp.list("openlight")) do
+			cmp.proxy(addr).setBrightness(brightness)
+	end
 end
 
-function TurnLightControl()
-	local obj = object.light
-	local Brightness = 0
-	if obj.state then
-		obj.state = false
-		Brightness = 0
-	else
-		obj.state = true
-		Brightness = 15
+local function redstoneControl(objectRow, newState)
+	local newLevel = 0
+	if newState then
+		newLevel = 15
 	end
-	for addr in pairs(cmp.list("openlight")) do
-			cmp.proxy(addr).setBrightness(Brightness)
-	end
+	objectRow.redstone.setOutput(objectRow.side, newLevel)
 end
 
 local function printMenu()
-	for name in pairs(object) do
-		print(name.." состояние:"..name.state)
+	term.clear()
+	for key, value in pairs(objects) do
+		io.stdout:write("[key:"..value.key.."] "..value.name..". state "..value.redstone.getOutput(value.side).."\n")
 	end
-
+	--light row
+	io.stdout:write("[key:l] ligth. state: "..tostring(light.state).."\n")
+	io.stdout:write("[key:q] quit programm\n")
 end
 
-local function inputProcessing()
-
-
+local function init()
+	for key, value in pairs(objects) do
+		objects[key].redstone = cmp.proxy(cmp.get(value.redstone))
+		redstoneControl(value, value.state)
+	end
+	for addr in pairs(cmp.list("openlight")) do
+		cmp.proxy(addr).setColor(0xFFFFFF)
+	end
 end
 
+local function eventHandler(...)
+	local event = {...}
+	for key, value in pairs(keycodes) do
+		if event[4] == value[1] then
+			--special for exit and light
+			if value[2] == "q" then
+				exitF = true
+				require("event").cancel(eventNumb)
+				return
+			elseif value[2] == "l" then
+				turnLight()
+				printMenu()
+				return
+			end
+			--search in objects table
+			for key, value in pairs(objects) do
+				if value.key == value[2] then
 
-while true do
-	
-	
-	
-	
+					printMenu()
+					return
+				end
+			end
+		end
+	end
 end
 
-term.clear()
+init()
+printMenu()
+eventNumb = require("event").listen("key_down", eventHandler)
+
+while not exitF do
+	os.sleep(1)
+end
