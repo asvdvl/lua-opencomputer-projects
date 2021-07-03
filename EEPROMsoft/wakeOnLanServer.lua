f6e776e1a76f1aa7db67208690dfb8b60982d3b2
asvdev <57688070+asvdvl@users.noreply.github.com>
2021-07-03 23:17:49 +0300

    Added listening for WOL packets (useful for backup WOL servers) Added print current uptime on clipboard(to determine the state of MC)

--- EEPROMsoft/wakeOnLanServer.lua
+++ EEPROMsoft/wakeOnLanServer.lua
@@ -1,7 +1,10 @@
---for eeprom
 local modem = component.proxy(component.list("modem")())
+local sign = component.proxy(component.list("sign")())
+
+modem.setWakeMessage("wakeup", true)
 
 while true do
     modem.broadcast(65535, "wakeup", computer.uptime())
+    sign.setValue("Uptime:\n"..tostring(computer.uptime()))
     computer.pullSignal(5)
 end
