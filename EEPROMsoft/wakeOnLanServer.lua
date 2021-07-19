89df86c6631784a480809f2685e841c00c048990
asvdev <57688070+asvdvl@users.noreply.github.com>
2021-07-19 23:06:35 +0300

    Added wakeup on redstone change.

--- EEPROMsoft/wakeOnLanServer.lua
+++ EEPROMsoft/wakeOnLanServer.lua
@@ -1,7 +1,9 @@
 local modem = component.proxy(component.list("modem")())
 local sign = component.proxy(component.list("sign")())
+local redstone = component.proxy(component.list("redstone")())
 
 modem.setWakeMessage("wakeup", true)
+redstone.setWakeThreshold(1)
 
 while true do
     modem.broadcast(65535, "wakeup", computer.uptime())
