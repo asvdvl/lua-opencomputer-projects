ff2db5841a20b4180bd026ed81932c81dd617141
asvdev <57688070+asvdvl@users.noreply.github.com>
2021-08-28 01:13:32 +0300

    Create small program for print some stats from asv.net library

---
+++ asv.net soft/nstat.lua
@@ -0,0 +1,31 @@
+local net = require("asv").net
+--local shell = require("shell")
+
+--local args, options = shell.parse(...)
+
+local fileName = os.tmpname()
+local statsFileWriter = io.open(fileName, "w")
+
+local function printRow(text)
+    statsFileWriter:write(text.."\n")
+end
+
+local function collectData()
+    printRow("phys level")
+    for key, value in pairs(net.phys.service.stats) do
+        printRow(key..": \t"..value)
+    end
+    printRow("")
+
+    printRow("LinkLayer level")
+    for key, value in pairs(net.LL.service.stats) do
+        printRow(key..": \t"..value)
+    end
+    printRow("")
+end
+
+collectData()
+
+statsFileWriter:close()
+loadfile("/bin/edit.lua")(fileName)
+require("filesystem").remove(fileName)
