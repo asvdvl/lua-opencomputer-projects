7c90a292ddd3290afc1293b9685efaba1bde1f4a
asvdev <57688070+asvdvl@users.noreply.github.com>
2023-02-01 20:24:12 +0300

    added stop by ctrl+c

--- soft/TPSCalculator.lua
+++ soft/TPSCalculator.lua
@@ -1,5 +1,6 @@
 local time = require("asv").time
 local cmp = require("component")
+local event = require("event")
 local gpu = cmp.gpu
 
 local screenW = 9
@@ -7,6 +8,8 @@ local screenH = 2
 local calculateTicks = 20    --5 ticks - closest to real value and low latency. 20 ticks - 1 second
 local colorOutput = true
 local fullsreen = true
+local interruptListen
+local exitF
 
 local function getTicks()
     return os.time()/3.6
@@ -17,7 +20,19 @@ if fullsreen then
     gpu.setResolution(screenW, screenH)
 end
 
-while true do
+local function stopAll()
+    print("bye!")
+    event.cancel(interruptListen)
+    exitF = true
+
+    if fullsreen then
+        gpu.setResolution(gpu.maxResolution())
+    end
+end
+
+interruptListen = event.listen("interrupted", stopAll)
+
+while not exitF do
     local realTime = time.getRaw()
     local gameTime = getTicks()
 
