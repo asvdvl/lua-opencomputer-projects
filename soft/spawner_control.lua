c6a424f395e8b77ef04dae048e4e0951c99dbdf9
asvdev <57688070+asvdvl@users.noreply.github.com>
2023-08-30 15:13:27 +0300

    rewritten for keyboard library optimize user list, remove all things that can be by default

--- soft/spawner_control.lua
+++ soft/spawner_control.lua
@@ -2,51 +2,57 @@ local cmp = require("component")
 local term = require("term")
 local exitF
 local eventNumb = 0
-local light = {state = false}
 local chars = {"-", "\\", "|", "/"}
 local currentChar = 0 --range 1-4
-local keycodes = { --4 parameter of key_down and key_up events
-	{2, "1"}, 	--{keycode, "real key"}
-	{3, "2"},
-	{4, "3"},
-	{5, "4"},
-	{6, "5"},
-	{7, "6"},
-	{8, "7"},
-	{9, "8"},
-	{10, "9"},
-	{11, "0"},
-	{38, "l"},
-	{16, "q"}
-}
+local keyb = require("keyboard")
 
 --user settings
-local objects = {
+local list = {
 	--name - visible name
-	--redstone - address of redstone interface
-	--key - real keyboard key(see keycodes table)
-	--side - side of redstone interface
-	--state - start redstone state
-    {name = "ifrits", redstone = "d31", key = "1", side = 5, state = false},
-    {name = "endermans", redstone = "d31", key = "2", side = 4, state = false},
-	{name = "wither skelets", redstone = "d31", key = "3", side = 3, state = false},
-	{name = "funs", redstone = "d31", key = "4", side = 0, state = true}
-
+	--redstone - address of redstone interface (OPTIONAL! if not, it will find primary and use it, but i recommend specifying it because "OC" can change it if you add 1 more interface)
+	--key - real keyboard key (OPTIONAL! do not set in range 1-9, 0!(i'm just too lazy for check already bind keys), or set all on your own)
+	--side - side of redstone interface, 0 - Down, 1 - Up, 2 - North, 3 - South, 4 - West, 5 - East
+	--state - start redstone state (OPTIONAL! by default in false)
+	
+	--[[here is a line with all possible parameters
+	{name = "SMTH ctrl", redstone = "123", key = "z", side = 0, state = true},
+	]]
+	{name = "Spw_N", side = 2},
+	{name = "Spw_S", side = 3},
+	{name = "Spw_W", side = 4},
+	{name = "Spw_E", side = 5},
+	{name = "light", redstone = "053", key = "l", side = 1, state = true},
 }
 --end user settings
 
-local function turnLight()
-	local brightness = 0
-	if light.state then
-		brightness = 0
-	else
-		brightness = 15
+assert(cmp.isAvailable("redstone"), "no redstone found")
+
+local iList = keyb.keys["1"]
+for _, lst in pairs(list) do
+	if not lst.redstone then
+		lst.redstone = cmp.redstone.address
 	end
-	light.state = not light.state
-	for addr in pairs(cmp.list("openlight")) do
-			cmp.proxy(addr).setBrightness(brightness)
+	if not lst.key then
+		lst.key = keyb.keys[iList]	--such a double conversion will allow you to include the number "0" in the list without performing additional checks at the generation stage
+		iList = iList + 1
+		if iList > keyb.keys["0"] then	--I assume that in the future the order of the keys (1-9, 0) will not change.
+			return
+		end
+	end
+	if not lst.state then	--allows not to write "state = false" in the list
+		lst.state = false
+	end
+
+	--I assume that name and side are always specified, this code serves as protection from the user
+	if not lst.name then
+		lst.name = "name not specifying!"
+	end
+	if not lst.side then
+		lst.side = 0
+		lst.name = lst.name + " / side not shown! taken by default for the bottom side"
 	end
 end
+iList = nil
 
 local function redstoneControl(objectRow, newState)
 	local newLevel = 0
@@ -67,18 +73,16 @@ end
 
 local function printMenu()
 	term.clear()
-	for key, value in pairs(objects) do
+	for _, value in pairs(list) do
 		io.stdout:write("[key:"..value.key.."] "..value.name..". state "..value.redstone.getOutput(value.side).."\n")
 	end
-	--light row
-	io.stdout:write("[key:l] ligth. state: "..tostring(light.state).."\n")
 	io.stdout:write("[key:q] quit programm\n")
 	io.stdout:write(justAStrangeActivityIndicator())
 end
 
 local function init()
-	for key, value in pairs(objects) do
-		objects[key].redstone = cmp.proxy(cmp.get(value.redstone))
+	for key, value in pairs(list) do
+		list[key].redstone = cmp.proxy(cmp.get(value.redstone))
 		redstoneControl(value, value.state)
 	end
 	for addr in pairs(cmp.list("openlight")) do
@@ -88,26 +92,18 @@ end
 
 local function eventHandler(...)
 	local event = {...}
-	for _, keyvalue in pairs(keycodes) do
-		if event[4] == keyvalue[1] then
-			--special for exit and light
-			if keyvalue[2] == "q" then
-				exitF = true
-				require("event").cancel(eventNumb)
-				return
-			elseif keyvalue[2] == "l" then
-				turnLight()
-				printMenu()
-				return
-			end
-			--search in objects table
-			for _, objvalue in pairs(objects) do
-				if objvalue.key == keyvalue[2] then
-					redstoneControl(objvalue, not objvalue.state)
-					printMenu()
-					return
-				end
-			end
+	if keyb.keys[event[4]] == "q" then --special for exit(may be used for other tasks)
+		exitF = true
+		require("event").cancel(eventNumb)
+		printMenu()
+		return
+	end
+	
+	for _, obj in pairs(list) do
+		if obj.key == keyb.keys[event[4]] then
+			redstoneControl(obj, not obj.state)
+			printMenu()
+			return
 		end
 	end
 end
@@ -117,5 +113,5 @@ printMenu()
 eventNumb = require("event").listen("key_down", eventHandler)
 
 while not exitF do
-	os.sleep(1)
+	os.sleep(0.2)
 end
