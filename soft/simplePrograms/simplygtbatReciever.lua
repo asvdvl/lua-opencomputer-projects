0ad1790aa8e5b7c43640d6022b06fedfc21d4c31
asvdev <57688070+asvdvl@users.noreply.github.com>
2024-03-08 18:47:55 +0300

    I wrote this so long ago that I don’t even remember what I changed, so I’ll leave it as it is Also, in addition to a simple update, there is also added output to the chat, for good reason these should be 2 different programs, but I will not separate them back

--- soft/simplePrograms/simplygtbatReciever.lua
+++ soft/simplePrograms/simplygtbatReciever.lua
@@ -1,21 +1,52 @@
+local time = require("asv").time
+local cmp = require("component")
+local event = require("event")
+local gpu = cmp.gpu
+
+local screenW = 15
+local screenH = 7
+local calculateTicks = 20    --5 ticks - closest to real value and low latency. 20 ticks - 1 second
+local colorOutput = true
+local fullsreen = true
+local interruptListen
+local exitF
+
+---
+
+local chat = cmp.chat
+local prewPerc = 0
+local currPerc = 0
+
+cmp.chat.setName("dial-up notice")
+cmp.chat.setDistance(3)
+
+---
+
 local asv = require("asv")
 local link = asv.net.Layers.Link
 local utils = asv.utils
+local time = asv.time
 local protoName = "gt_batteryAdv"
+local lastTake = 0
 
 local packetStruc = {
-    type = "none",
-    stored = {
-        current = 0,
-        max = 0
-    },
-    flow = {
-        input = 0,
-        output = 0
+        type = "none",
+        stored = {
+            current = 0,
+            max = 1,
+            percent = 0
+        },
+        flow = {
+            input = 0,
+            output = 0
+        }
     }
-}
+
+local row1 = ""
+local row2 = ""
 
 --register proto
+local i = 0
 link.protocols[protoName] = {
     onMessageReceived = function(dstAddr, frame, srcAddr)
         local data, bagRequest = utils.correctTableStructure(frame.data, packetStruc)
@@ -25,17 +56,76 @@ link.protocols[protoName] = {
         end
 
         if data.type == "response" then     --do something with this data
-            print("report:")
-            print("buff: "..tostring(data.stored.current).."/"..tostring(data.stored.max))
-            print("flow: in "..tostring(data.flow.input).."; out "..tostring(data.flow.output))
+            i = i - 1
+            currPerc = data.stored.percent
+            row1 = "buff: "..tostring(currPerc).."%"
+            row2 = "flow:\nin "..tostring(data.flow.input).."\nout "..tostring(data.flow.output)
+            lastTake = time.getRaw()
+
+            if i < 0 then
+                chat.say("§r§2"..row1.."§f(§3"
+                  ..tostring(currPerc - prewPerc).."§f)§c "
+                  ..tostring(data.flow.input).."§f/§a"
+                  ..tostring(data.flow.output).." §f(§3"
+                  ..tostring(data.flow.input-data.flow.output).."§f)")
+                i = 6
+                prewPerc = currPerc
+            end
         end
     end
 }
 
-for i = 1, 3 do --"main loop"
-    link.broadcast(nil, protoName, {type = "request"})
-    os.sleep(1)
+---
+
+local function getTicks()
+    return os.time()/3.6
 end
 
---unregister proto on close programm(this step is required)
+if fullsreen then
+    require("term").clear()
+    gpu.setResolution(screenW, screenH)
+end
+
+local function stopAll()
+    print("closing")
+    event.cancel(interruptListen)
+    exitF = true
+
+    if fullsreen then
+        gpu.setResolution(gpu.maxResolution())
+    end
+end
+
+interruptListen = event.listen("interrupted", stopAll)
+local ti = false
+
+while not exitF do
+    local realTime = time.getRaw()
+    local gameTime = getTicks()
+
+    --sleep on in-game ticks
+    while (getTicks() - gameTime) < calculateTicks do
+        os.sleep(0)
+    end
+    local tps = math.ceil(10000/((time.getRaw() - realTime)/(getTicks() - gameTime)))/10
+
+    --set collor
+    if colorOutput then
+        if tps <= 12 then
+            gpu.setForeground(0xFF0000)
+        elseif tps <= 15 then
+            gpu.setForeground(0xFFFF55)
+        else
+            gpu.setForeground(0x55FF55)
+        end
+    end
+
+    print("tps: "..tps)
+    gpu.setForeground(0xFFFFFF)
+    print(row1)
+    print(row2)
+    print("upd: "..tostring(math.ceil((time.getRaw()-lastTake)/1000)).." ago")
+end
+
+print("bye!")
 link.protocols[protoName] = nil
